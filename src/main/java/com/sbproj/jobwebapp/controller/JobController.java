a0b133644fc354a3f793e54972b9c3a6074a713e
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-04-09 21:02:23 +0530

    used data jpa

--- src/main/java/com/sbproj/jobwebapp/controller/JobController.java
+++ src/main/java/com/sbproj/jobwebapp/controller/JobController.java
@@ -4,67 +4,50 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.sbproj.jobwebapp.model.JobPost;
 import com.sbproj.jobwebapp.service.JobService;
 
-@Controller
+@RestController
 public class JobController {
 
-	@Autowired
-	private JobService service;
-
-	// ************************************************************************
-
-	@RequestMapping({"/","/home"})
-	public String home() {
-		return "home";
-	}
-
-	// ************************************************************************
-
-	@RequestMapping("/addjob")
-	public String addJob() {
-		return "addjob";
-	}
-
-	// ************************************************************************
-
-	// controller method for getting all job posts
-	@GetMapping("/viewalljobs")
-	public String viewJobs(Model model) {
-
-		List<JobPost> jobPosts = service.returnAllJobPosts();
-		model.addAttribute("jobPosts", jobPosts);
-		return "viewalljobs";
-	}
-
-	// ************************************************************************
-
-	@PostMapping("/handleForm")
-	public String handleAddJobForm(JobPost jobPost,Model model) {
-		model.addAttribute("jobPost", jobPost);
-		service.addJobPost(jobPost);
-		//System.out.println(jobPost);
-		  return "success";
-
-	}
+    @Autowired
+    private JobService service;
 
 
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
+    }
 
+    @GetMapping("/jobPost/{postId}")
+    public JobPost getJob(@PathVariable int postId) {
+        return service.getJob(postId);
+    }
 
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost) {
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
 
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
 
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable int postId) {
+        service.deleteJob(postId);
+        return "Deleted";
+    }
 
-
-
-
-
-
-
+    @GetMapping("load")
+    public String loadData() {
+        service.load();
+        return "success";
+    }
 }
